0cced1eee9e9029b6b810e1a7b705dc4a463e14b
student <student@192.168.101.22>
2016-03-16 16:08:23 +0100

    testweiser Commit

--- src/de/hs_mannheim/i/imb/ss16/tpe/group9/Aufgabe_1/schnelleKlasse.java
+++ src/de/hs_mannheim/i/imb/ss16/tpe/group9/Aufgabe_1/schnelleKlasse.java
@@ -1,5 +1,24 @@
 package de.hs_mannheim.i.imb.ss16.tpe.group9.Aufgabe_1;
 
 public class schnelleKlasse {
+		private String name;
+		private String kuerzel;
+		private String kurs;
+		
+		public String toString(){
+			return name + " " + kuerzel + " " + kurs;
+		}
+		
+		public String getName(){
+			return name;
+		}
+		public String getKuerzel(){
+			return kuerzel;
+		}
+		public String getKurs(){
+			return kurs;
+		}
+		
+			
 
 }
